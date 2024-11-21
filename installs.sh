fd4219bff966d3b5117af297fd0fe382e41cf7c0
lucas <lucasycosta@gmail.com>
2024-11-21 12:33:22 -0300

    estrutura

---
+++ installs.sh
@@ -0,0 +1,32 @@
+#JAVA
+sudo apt update
+sudo apt install openjdk-17-jdk -y
+
+#GIT
+sudo apt install git -y
+git config --global user.name "Zeus"
+git config --global user.email "zeussoftwarehouse@gmail.com"
+
+#DOCKER
+sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+#JENKINS
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+#sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+
+
